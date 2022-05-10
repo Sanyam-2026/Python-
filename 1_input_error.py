10dd0dbb8dd61248037a2e6db03684e4f4c16baf
Sanyam-2026 <99472914+Sanyam-2026@users.noreply.github.com>
2022-05-10 23:51:55 +0530

    Add files via upload

---
+++ 1_input_error.py
@@ -0,0 +1,18 @@
+def user_input():
+    # a= int(input("enter value 1 "))
+    # b=int(input("enter value 2 "))
+    a,b= int(input("Enter val 1 :")), int(input("Enter val 2 :"))
+    
+    return (a,b)
+def add(a,b):
+    return a+b
+a,b = user_input()
+#add(a,b)
+print(add(a,b))
+
+
+
+
+#----------------------- Error in this program \
+#   Name Error  
+
